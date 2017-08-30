ea01d979827d015f97a7621256823ee36d3308dd
DESKTOP-AMS26LB\AI <aizhengpeng@yeah.net>
2017-08-30 19:34:00 +0800

    Create RC4.py

--- CipherNEU/Cipher/Caesar.py
+++ CipherNEU/Cipher/Caesar.py
@@ -3,8 +3,8 @@ Caesar Cipher
 Programmer: Ai Zhengpeng
 Date: 2017-08-30
 Function:
-Encrypt(plaintext, key)
-Decrypt(ciphertext, key)                                                    
+Encrypt(plaintext, key) -> ciphertext
+Decrypt(ciphertext, key) -> plaintext
 '''
 
 def Encrypt(plaintext, key):
