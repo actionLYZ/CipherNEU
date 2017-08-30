5027c9ff8cb4c1bc73bbe878a9af73abc6f603d6
LAPTOP-KLKRA659\GQY <645591456@qq.com>
2017-08-30 20:42:58 +0800

    更新了column

--- CipherNEU/Cipher/Coloum.py
+++ CipherNEU/Cipher/Coloum.py
@@ -2,17 +2,24 @@
 
 # define function
 def TranspositionCipher(key, message):  
-
+    """ 
+    @param key: a positive number used for encryption 
+    @param message: the plain text needs encryption 
+    @return: the encrypted text 
+    """  
     if key <= 0: # the algorithm can't work
         print("key is less than 0,can not work")
         return message   
-    else: 
-        # 返回加密后的密文 
+    else:  
         return "".join([message[i::key] for i in range(key)])
 
 
 def DecryptTranspositionCipher(key, message):  
-
+    """ 
+    @param key: a positive number used for encryption 
+    @param message:  the encrypted message 
+    @return: plaintext 
+    """  
     import math  
     numOfColumns = math.ceil(len(message) / key)  
     numOfRows = key  
@@ -27,15 +34,13 @@ def DecryptTranspositionCipher(key, message):
       
         if (col == numOfColumns) or (col == numOfColumns - 1 and row >= numOfRows - numOfShadeBoxes):  
             col = 0  
-            row += 1
-    # 返回解密后的明文  
+            row += 1  
     return ''.join(plaintext)  
 
 plaintext = input("Input the plaitext \n")
 key = int(input("Input the key \n"))
 
 # call function
-# 列置换加密
 Ciphertext = TranspositionCipher(key,plaintext)
 
 print ("The Ciphertext is %s" % Ciphertext)
