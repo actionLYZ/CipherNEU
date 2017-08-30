b0f6b1d6177509e22d6a534e5eac5422fe535e99
DESKTOP-AMS26LB\AI <aizhengpeng@yeah.net>
2017-08-30 16:31:20 +0800

    Original Project Files

---
+++ CipherNEU/Cipher/Caesar.py
@@ -0,0 +1,18 @@
+'''
+Caesar Cipher
+Programmer: Ai Zhengpeng
+Date: 2017-08-30
+Function:
+Encrypt(plaintext, key)
+Decrypt(ciphertext, key)                                                    
+'''
+
+def Encrypt(plaintext, key):
+    ciphertext = ""
+
+    return ciphertext
+
+def Decrypt(ciphertext, key):
+    plaintext = ""
+
+    return plaintext
