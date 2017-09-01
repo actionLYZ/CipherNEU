eb97e67556b45a44ceba07162ed5fcf7831857e3
actionLYZ <13940220020@gmail.com>
2017-09-01 16:45:56 +0800

    加入了playfair解密

--- CipherNEU/Cipher/PlayFair.py
+++ CipherNEU/Cipher/PlayFair.py
@@ -24,8 +24,21 @@ def Encrypt(plainText,key):
     cipherText = Plain2Cipher(plainText,keyList)    #将明文加密成密文
     return cipherText
 
-    '''-----------------------------------------------------------------------------------
-函数定义	：    规范明文格式
+'''-----------------------------------------------------------------------------------
+函数定义	：    加密PlayFair密码
+函数参数	：    plaintext(string)为明文,key为密钥(string)
+函数返回值  ：    加密后结果(string)
+--------------------------------------------------------------------------------------'''
+def Decrypt(cipherText,key):
+    plainText = ""
+    keyList = makeKeyList(key)                      #生成密钥二维列表
+    plainText = Cipher2Plain(cipherText,keyList)    #将密文解密成明文
+    plainText = DeNorm(plainText)                   #反规范明文格式
+    return plainText
+
+
+'''-----------------------------------------------------------------------------------
+函数定义	：    规范明文格式（将可读的明文变为可加密的明文）
 函数参数	：    plaintext(string)为明文
 函数返回值  ：    (string)plainText :  规范后明文
                   (Bool)ifAdd       :  是否在尾部加入了'q'，是为true
@@ -61,6 +74,25 @@ def Norm(plainText):
 
     else:
         return (plainText,False)
+
+'''-----------------------------------------------------------------------------------
+函数定义	：    反规范明文格式（将可加密的明文变为可读的明文）
+函数参数	：    plaintext(string)为明文
+函数返回值  ：    (string)plainText :  反规范后明文
+规范方式    ：    当q两边字母相同时，删掉q。当最后一个字符为q时，删除q
+--------------------------------------------------------------------------------------'''
+def DeNorm(plainText):
+    plainText = list(plainText)
+    num = 1
+    while num < len(plainText) - 1 :
+        if plainText[num] == 'q' and plainText[num-1] == plainText[num+1]:
+            del plainText[num]
+        num += 1
+
+    if plainText[-1] == 'q':
+        del plainText[-1]
+    plainText = ''.join(plainText)
+    return plainText
 
 '''-----------------------------------------------------------------------------------
 函数定义	：    生成密钥二维列表
@@ -158,8 +190,49 @@ def Plain2Cipher(plainText,keyList):
             exchange = 0;
 
     cipherTextstr = ''.join(cipherText)
-    print(cipherTextstr)
     return cipherTextstr
+
+'''-----------------------------------------------------------------------------------
+函数定义	：    将密文解密成明文
+函数参数	：    cipherText(string)   :   密文
+                  keyList(list)        :   密钥二维列表
+函数返回值  ：    明文(string)
+--------------------------------------------------------------------------------------'''
+def Cipher2Plain(cipherText,keyList):
+    plainText = []
+
+    # 将明文长度与密文一致，并且初始化为0
+    for i in range(len(cipherText)):
+        plainText.append(0)
+
+    exchange = 0
+    for loc in range(len(cipherText)):
+
+        #如果不是字母，直接赋值给明文
+        if cipherText[loc].isalpha() == False:
+             plainText[loc] = cipherText[loc]
+             continue
+
+         #如果是字母，依次分配给letter1和letter2
+        if exchange == 0:
+            letter1 = cipherText[loc]
+            loc1 = loc
+            exchange = 0.5
+        elif exchange == 0.5:
+            letter2 = cipherText[loc]
+            loc2 = loc
+            exchange = 1
+
+        #将两个字母依据规则放到明文
+        if exchange == 1:
+            (letter1,letter2) = ChangePlainText(letter1,letter2,keyList)
+            plainText[loc1] = letter1
+            plainText[loc2] = letter2
+            exchange = 0;
+
+    plainTextstr = ''.join(plainText)
+    return plainTextstr
+
 
 '''-----------------------------------------------------------------------------------
 函数定义	：    将两个字母依据规则变换成密文中的对应字母
@@ -188,6 +261,35 @@ def ChangeCipherText(letter1,letter2,keyList):
         letter1 = keyList[locx1][locy2]
         letter2 = keyList[locx2][locy1]
     return (letter1,letter2)
+
+'''-----------------------------------------------------------------------------------
+函数定义	：    将两个字母依据规则变换成明文中的对应字母
+函数参数	：    letter1,letter2(string)   :   两个字母
+                  keyList(2Dlist)           ;   密钥二维数组
+函数返回值  ：    明文中的对应的两个字母
+--------------------------------------------------------------------------------------'''
+def ChangePlainText(letter1,letter2,keyList):
+
+    #找到两个字母在密钥列表中的坐标
+    (locx1,locy1) = FindLocOfList(letter1,keyList)
+    (locx2,locy2) = FindLocOfList(letter2,keyList)
+
+    #如果在同一行
+    if locx1 == locx2:
+        letter1 = keyList[locx1][(locy1 - 1) % 5]
+        letter2 = keyList[locx2][(locy2 - 1) % 5]
+
+    #如果在同一列
+    elif locy1 == locy2:
+        letter1 = keyList[(locx1 - 1) % 5][locy1]
+        letter2 = keyList[(locx2 - 1) % 5][locy2]
+
+    #如果既不在同一行也不再同一列
+    else:
+        letter1 = keyList[locx1][locy2]
+        letter2 = keyList[locx2][locy1]
+    return (letter1,letter2)
+
 '''-----------------------------------------------------------------------------------
 函数定义	：    找到一个字母在一个二维数组中的坐标
 函数参数	：    letter1(string)   :   字母
